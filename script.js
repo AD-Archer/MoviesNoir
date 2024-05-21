50d8b8ce3203af41dcaf5560ee9974b1b26c1cde
Archer <89430358+bsanx@users.noreply.github.com>
2024-05-21 12:45:18 -0400

    Create script.js
    created a script in Js to link to google as a test for the front page

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.link-button').addEventListener('click', function() {
+  window.location.href = 'https://www.google.com'; // Replace 'YOUR_LINK_HERE' with the actual URL
+});
