1a7f630dc44004e0e8d6bae14cb4c7d8d353a08e
Archer <89430358+AD-Archer@users.noreply.github.com>
2025-01-30 12:28:07 -0500

    update to geneator.jsx

--- src/pages/Generator.jsx
+++ src/pages/Generator.jsx
@@ -4,30 +4,31 @@ const Generator = () => {
   return (
     <Container className="generator-container">
       <header className="text-center py-4">
-        <h1>Random Media Generator</h1>
-        <p>Press a button, and find something to watch:</p>
+        {/* <h1>Random Media Generator</h1> */}
+        <h1>Press a button, and find something to watch:</h1>
       </header>
 
       <div className="generator-frame">
         <iframe 
           src="https://ad-archer.github.io/MoviesNoir-Movie-Generator/" 
+          // src='http://127.0.0.1:5500/index.html' used for development
           title="Movie Generator"
           className="generator-iframe"
         />
       </div>
 
       <div className="info-section">
-        <p className="info-bubble">
-          Here is some additional information about the embedded content.
-        </p>
-        <a 
+        {/* <p className="info-bubble">
+          This generator .
+        </p> */}
+        {/* <a 
           href="https://ad-archer.github.io/MoviesNoir-Movie-Generator/" 
           className="custom-button"
           target="_blank"
           rel="noopener noreferrer"
         >
           Visit Generator
-        </a>
+        </a> */}
       </div>
     </Container>
   );
